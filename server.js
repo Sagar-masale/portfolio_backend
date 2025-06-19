1a993416e4a7e3b26c67bdd6a7fb1657c22f3c31
sagar-masale <sagarmasale27@gmail.com>
2025-06-19 20:15:10 +0530

    update package files

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_ORIGIN,
+  credentials: true,
+}));
 app.use(express.json());
 
 app.post("/api/send", async (req, res) => {
